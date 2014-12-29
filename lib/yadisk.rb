bd5b25ead69ae714ab08c3755f43513efab7e75a
Ilya Vasilenkov <wim-norder@yandex.com>
2014-12-29 16:27:03 +0300

    Update yadisk.rb
    API overview

--- lib/yadisk.rb
+++ lib/yadisk.rb
@@ -3,7 +3,28 @@
     p "Yadisk init"
   end
   
-  def self.login
+  def self.login 
     p "logged in..."
   end
+  
+  def self.mkdir (path)
+    p "Making #{path}"
+  end
+  
+  def self.rm (path)
+    p "#{path} removed ..."
+  end
+  
+  def self.get (path)
+    p "Getting #{path}"
+  end
+  
+  def self.put (path)
+    p "Putting #{path}"
+  end
+  
+  def exists? (path)
+    p "exist || or not"
+  end
+  
 end
